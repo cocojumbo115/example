edd513430ff730dcfa7d41260baa04204dbe330f
Maksim <maksim.permyakov@fojin.tech>
2025-11-20 12:29:06 +0300

    сделал основу для ui-тестов

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -1,13 +1,22 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
+import static com.codeborne.selenide.Configuration.pageLoadStrategy;
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SearchTests {
+
+    @BeforeAll
+    public static void setup() {
+        pageLoadStrategy = "eager"; // не ждем полной загрузки страницы
+    }
     @Test
     void successfulSearchTest() {
-        open("https://www.google.com/");
-        $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://selenide.org"));
+        open("https://qa.guru/");
+        $(".text-block-14").shouldBe(visible).click();
+
+
+
     }
 }
